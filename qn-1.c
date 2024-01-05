2584e7b927f125e67245e5c123eb6be763db799d
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-01-05 16:15:42 +0530

    Update qn-1.c

--- qn-1.c
+++ qn-1.c
@@ -1,10 +1,23 @@
-//1. Define a structure Employee with member variables id, name, salary
-
+//1. Define a structure Employee with member variables id, name, salary.
 
 #include<stdio.h>
+#include<string.h>
 struct employee
 {
-    int id;
-    char name[50];
-    float salary;
+   int id;
+   char name[20];
+   float salary;
+     
 };
+int main()
+{
+    struct employee man;
+    printf("enter id, name and salary:\n");
+    scanf("%d",&man.id);
+    fflush(stdin);
+    fgets(man.name,20,stdin);
+    man.name[strlen(man.name)-1]='\0';
+    scanf("%f",&man.salary);
+
+    printf("%d %s %f",man.id,man.name,man.salary);
+}
